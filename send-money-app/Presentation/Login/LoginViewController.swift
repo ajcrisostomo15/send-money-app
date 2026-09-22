f5a00ef89961a6d4d8c749a3a04479d182640644
Allen Crisostomo <allen.crisostomo@novare.com.ph>
2026-09-23 01:04:46 +0800

    added login action to login button

--- send-money-app/Presentation/Login/LoginViewController.swift
+++ send-money-app/Presentation/Login/LoginViewController.swift
@@ -28,9 +28,24 @@ class LoginViewController: UIViewController {
         let button = UIButton(type: .roundedRect)
         button.setTitle("Login", for: .normal)
         button.setTitleColor(.black, for: .normal)
+        
+        let action = UIAction(handler: { [weak self] _ in
+            self?.onLoginTapped()
+        })
+        
+        button.addAction(action, for: .touchUpInside)
         return button
     }()
     
+    private let viewModel: LoginViewModel
+    init(viewModel: LoginViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -65,6 +80,12 @@ class LoginViewController: UIViewController {
             make.left.equalToSuperview().offset(padding)
             make.right.equalToSuperview().inset(padding)
         }
+    }
+    
+    private func onLoginTapped() {
+        viewModel.login(username: usernameTextField.text ?? "",
+                        password: passwordTextField.text ?? ""
+        )
     }
 }
 
