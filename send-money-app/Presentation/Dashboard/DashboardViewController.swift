6535caaedd2dddd595682dc21154c892cdf90502
Allen Crisostomo <allen.crisostomo@novare.com.ph>
2026-09-23 01:47:31 +0800

    initialize viewmodel in vc

--- send-money-app/Presentation/Dashboard/DashboardViewController.swift
+++ send-money-app/Presentation/Dashboard/DashboardViewController.swift
@@ -93,6 +93,17 @@ class DashboardViewController: UIViewController {
         stackView.spacing = 16
         return stackView
     }()
+    
+    private let viewModel: DashboardViewModel
+    
+    init(viewModel: DashboardViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
