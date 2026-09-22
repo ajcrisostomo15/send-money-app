8cf548400e20572b499c2ff4ac34ffceb9413168
Allen Crisostomo <allen.crisostomo@novare.com.ph>
2026-09-23 01:03:26 +0800

    added needed function for logging in

--- send-money-app/Presentation/Login/LoginViewModel.swift
+++ send-money-app/Presentation/Login/LoginViewModel.swift
@@ -8,11 +8,37 @@
 import Foundation
 
 class LoginViewModel {
-    init() {
-        
+    enum NetworkState {
+        case idle
+        case loading
+        case success
+        case failure(String)
+    }
+    
+    private let authRepository: AuthRepositoryProtocol
+    
+    public var onStateChange: ((NetworkState) -> Void)?
+    init(authRepository: AuthRepositoryProtocol) {
+        self.authRepository = authRepository
     }
     
     func login(username: String, password: String) {
+        onStateChange?(NetworkState.loading)
         
+        Task {
+            do {
+                let success = try await authRepository.login(username: username,
+                                                             password: password
+                )
+                
+                if success {
+                    onStateChange?(NetworkState.success)
+                } else {
+                    onStateChange?(.failure("Invalid username or password"))
+                }
+            } catch {
+                onStateChange?(.failure(error.localizedDescription))
+            }
+        }
     }
 }
