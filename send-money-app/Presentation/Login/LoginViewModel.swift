2ef69693c07d338052d528be54bca94ad4724558
Allen Crisostomo <allen.crisostomo@novare.com.ph>
2026-09-22 17:00:15 +0800

    added LoginViewModel class

---
+++ send-money-app/Presentation/Login/LoginViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewModel.swift
+//  send-money-app
+//
+//  Created by Allen Jeffrey Crisostomo on 9/22/26.
+//
+
+import Foundation
+
+class LoginViewModel {
+    init() {
+        
+    }
+    
+    func login(username: String, password: String) {
+        
+    }
+}
