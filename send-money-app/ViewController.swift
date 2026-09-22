08799540b7d68341f068f492ea1991f4907c0578
Allen Crisostomo <allen.crisostomo@novare.com.ph>
2026-09-22 15:41:49 +0800

    Initial Commit

---
+++ send-money-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  send-money-app
+//
+//  Created by Allen Jeffrey Crisostomo on 9/22/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
