043040f6d7fb58e2d6f2afb4e8bbfbf5126b7419
Allen Crisostomo <allen.crisostomo@novare.com.ph>
2026-09-23 00:36:15 +0800

    create user model

---
+++ send-money-app/Data/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  send-money-app
+//
+//  Created by Allen Jeffrey Crisostomo on 9/23/26.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: Int
+    let name: String
+    let email: String
+    let username: String
+}
