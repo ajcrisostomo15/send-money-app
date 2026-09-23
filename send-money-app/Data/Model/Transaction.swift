5bdd0c22e8b05eeeed24ef98a7eae4232b270024
Allen Crisostomo <allen.crisostomo@novare.com.ph>
2026-09-23 13:05:32 +0800

    created transactionRepository to handle transaction API request

---
+++ send-money-app/Data/Model/Transaction.swift
@@ -0,0 +1,8 @@
+//
+//  Transaction.swift
+//  send-money-app
+//
+//  Created by Allen Jeffrey Crisostomo on 9/23/26.
+//
+
+import Foundation
---
+++ send-money-app/Data/Repository/TransactionRepository.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionRepository.swift
+//  send-money-app
+//
+//  Created by Allen Jeffrey Crisostomo on 9/23/26.
+//
+
+import Foundation
